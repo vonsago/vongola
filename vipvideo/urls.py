e4ce86a78e81bdb727ea9414ebfe6388698f6c82
vonsago <f811194414@gmail.com>
2019-10-13 10:42:29 +0800

    add first ads

--- vipvideo/urls.py
+++ vipvideo/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('/', include('vipvideo.bolg.urls')),
+    path('', include('vipvideo.blog.urls')),
     path('video/', include('vipvideo.video.urls')),
     path('admin/', admin.site.urls),
 ]
